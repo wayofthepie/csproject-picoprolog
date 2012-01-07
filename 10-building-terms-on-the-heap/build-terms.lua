a94434b5fa79939ce850028eb907e0ac31f2f5b2
Stephen O'Brien <wayofthepie@gmail.com>
2012-01-07 13:07:23 +0000

    Begun work on functions to build terms on the heap.

---
+++ 10-building-terms-on-the-heap/build-terms.lua
@@ -0,0 +1,27 @@
+BuildTerms = {}
+HeapFunctions = {}
+
+function BuiltTerms.new() 
+    local self = {}
+    
+    function makeCompound(symbol, args)
+        
+    end
+    
+    function makeReference() 
+    end
+    
+    function makeInteger()
+    end
+    
+    function makeChar()
+    end
+    
+    function makeString()
+    end
+    
+    function makeClause()
+    end
+    
+    
+end
