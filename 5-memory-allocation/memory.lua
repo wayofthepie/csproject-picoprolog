87884acd7690dab5c2dcc885a82f9efbd71233f1
Stephen O'Brien <wayofthepie@gmail.com>
2012-01-07 13:09:30 +0000

    Begun work on functions to allocate memory.

--- 5-memory-allocation/memory.lua
+++ 5-memory-allocation/memory.lua
@@ -1,5 +1,41 @@
 Memory = {}
 
 function Memory.new() 
+    local self {}
+    
+    local localsp,globalsp,heapp,hmark
+    
+    --[[
+        Allocate space on the local stack.
+        @param Integer - size
+        @return - localsp + the size allocated
+    --]]
+    function locAlloc(size)
+        local temp
+        if localsp + size >= globalsp then 
+            -- TODO This should also kill interpreter.
+            error("Out of Stack space!!") 
+        end
+        temp = localsp + 1
+        localsp = localsp + size
+        return temp        
+    end
+    
+    --[[
+        Allocate space on the global stack.
+        @param Integer - kind
+        @param Integer - size
+        @return
+    --]]
+    function gloAlloc(kind, size)
+        local pointer
+        if globalsp - size < localsp then
+            -- TODO This should also kill interpreter.
+            error("Out of Stack space!!")
+        end
+        globalsp = globalsp - size
+        pointer = globalsp
+        -- TODO finish function: C.4
+    end
     
 end
