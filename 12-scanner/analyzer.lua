6abd4236003ffc35522801e4c9301f90acc97ffe
Stephen O'Brien <wayofthepie@gmail.com>
2012-01-15 18:57:25 +0000

    Updated analyzer.lua, cleaning up code.

--- 12-scanner/analyzer.lua
+++ 12-scanner/analyzer.lua
@@ -1,45 +1,21 @@
--- Create a module called analyzer
-module("analyzer",package.seeall)
+module("lexical-analyzer",package.seeall)
 
-package.path = package.path .. ";../0-prelude/?.lua;../lib/?.lua"
+-- Edit the package search path
+package.path = package.path .. ";../0-prelude/?.lua;../lib/?.lua;../lib/classes/?.lua"
 require "prelude" require "scanner" require("token-values")
- require "constant"
+require "constant" require "StrBuffer"
 
--- Construct a new scanner
 local scan = Scanner.new()
-
+local _tlib = require "_tlib"
 -- Load a file into the scanner
 scan:loadFile("test.pp")
---scan:printAll()
 
-local StrBuffer = {}
+
+
 --[[
-    Used to build up strings.
+    Class to represent tokens.
 --]]
-function StrBuffer.new()
-    local self = {}
-    local string = {}
-    local index = 1
-    
-    --[[
-        Appends the character 'char' to the end of this StrngBuffer.
-    --]]
-    function self:append(char)
-        string[index] = char
-        index = index + 1
-    end
-    
-    --[[
-    --]]
-    function self:toString()
-        return table.concat(string, "")
-    end
-    
-    return self
-end
-
-Token = {}
-
+local Token = {}
 function Token.new(token, value)
     local self = {}
     local t = token
@@ -56,200 +32,74 @@ function Token.new(token, value)
     return self
 end
 
-local tlib = require "tlib"
-
---print(scan:hasNext())
-function getNextToken()
-    local token = 0
-    local tokval 
-    local tokival = 0
-    local toksval = ""
-    local tokenlen = 0
-    local tokvar= ""
-    local sbuff = StrBuffer.new()
-    local num = 0
-    local char = scan:nextChar()
-    local rules = Rules.new()
-    
-    -- Should use an iterator here possibly..
-    while token == 0 and char ~= nil do
-        if char == SpecVals.ENDFILE then
-            token = TokVal.EOFTOK
-        elseif char == SpecVals.ENDLINE then
-            char = scan:nextChar()
-        -- Is the char a letter?
-        elseif string.find(char, '%a') ~= nil then
-            -- Is it an uppercase letter?
-            if string.find(char, '%u') ~= nil then
-                token = TokVal.VARIABLE
-            else 
-                token = TokVal.IDENT
-            end
-            tokenlen = 1
-            -- While it's a letter or digit
-            while (string.find(char, '%a') ~= nil) or (string.find(char, '%d') ~= nil) do
-                if tokenlen > TunableParameters.MAXSTRING then
-                    error("identifier too long!")
-                end               
-                sbuff:append(char)
-                tokenlen = tokenlen + 1
-                char = scan:nextChar()
-            end
-            
-            
-        -- Is it a number?
-        elseif type(char) == "number" then
-            token = TokVal.NUMBER
-            tokival = tokival * 10
-            while type(char) == "number" do
-                tokival = tokival + char
-                char = scan:nextChar()                
-            end    
-            
-            scan:dec()
-        else 
-            -- cases
-            local switch = {
-                ["("] = function() token = TokVal.LPAR;     print("(") end,
-                [")"] = function() token = TokVal.RPAR;     print(")")  end,
-                [","] = function() token = TokVal.COMMA;    print(",") end,
-                ["."] = function() token = TokVal.DOT       print(".") end,
-                ["="] = function() token = TokVal.EQUAL     print("=") end,
-                ["!"] = function() token = TokVal.IDENT; --[[ TODO tokval = cutsym --]] end,
-                ["/"] = function() 
-                            char = scan:nextChar()
-                            if char ~= "*" then
-                                error("bad token \"/\"")
-                            else 
-                                chartwo = ' '
-                                char = scan:nextChar()
-                                while scan:hasNext() and not(chartwo =='*' and char == "/") do
-                                    chartwo = char; char = scan:nextChar() 
-                                end
-                                if scan:hasNext() == nil then
-                                    error("end of file in comment!")
-                                else
-                                    char = scan:nextChar()
-                                end
-                            end
-                        end,
-                [":"] = function() 
-                            char = scan:nextChar()
-                            if char == "-" then
-                                token = TokVal.ARROW
-                            else
-                                scan:dec()
-                                token = TokVal.COLON
-                            end
-                        end,
-                ["\'"] = function()
-                             token = TokVal.CHCON
-                             -- TODO tokival = ascii rep of char ~IMPROVE
-                             scan:nextChar()
-                             char = scan:nextChar()
-                             if char ~= "\'" then
-                                 error("missing quote!")
-                             end
-                         end,
-                ["\""] = function()
-                             token = TokVal.STRCON 
-                             token = toklen + 1
-                             char = scan:nextChar()
-                             while char ~= "\"" and char ~= SpecVals.ENDLINE do
-                                 sbuff:append(char)
-                                 char = scan:nextChar()
-                                 tokenlen = tokenlen + 1
-                                 if char == SpecVals.ENDLINE then
-                                     error("unterminated string!")
-                                     scan:dec()
-                                 end
-                             end
-                         end        
-            }
-            -- Any access to an index that does not exist will return "illegal".
-           
-            tlib.setDefault(switch, "illegal")
-            
-            case = switch[char]
-            
-            if  case == "illegal" then 
-                error("illegal character " .. char .. "!")
-            else 
-                case()
-            end
-          
-        end
-
-         
-    end
-    return token        
-end
-
-function getToken2()
-    local token = Token.new(0,0)
-    local rules = Rules.new()
-    while token:getValue() == 0 do        
-        char = scan:currentChar()
-        r = rules:rules()
-        
-        token = r[char](char)
-        --print(token)
-        
-        --print(token:getType())
-        
-        --print(scan:currentChar())
-    end
-    return token
-end
-local lineno = 1
-Rules = {}
+--[[
+    Represents rules for characters being scanned.
+--]]
+local Rules = {}
 function Rules.new() 
     local self = {}
     
     local sbuff = StrBuffer.new()
-   
+
     local rules = {
+        --[[
+            Whitespace rules: 
+            Ignore, scan the next character and return a Token of type and value 0.
+        --]]
         [SpecVals.SPACE]    =   function() scan:nextChar() return Token.new(0,0) end,
         
+        --[[
+            End of file rules: 
+        --]]
         [SpecVals.ENDFILE]  =   function()
                                     return Token.new(TokVal.EOFTOK)
                                 end,
-                                
-        [SpecVals.ENDLINE]  =   function()                                   
+        
+        --[[
+            End of line rules:
+            TODO fix line number counting.
+        --]]
+        [SpecVals.ENDLINE]  =   function()     
+                                    local lineno = 0
                                     scan:nextChar() 
-                                    lineno = lineno + 1
+                                                                        
                                     print ("line num = " .. lineno)
-                                    return Token.new(TokVal.EOLTOK) 
+                                    return Token.new(TokVal.EOLTOK, lineno) 
                                 end,
-                                
+        
+        --[[
+            Rules for identifiers:
+        --]]
         ['IDENT']           =   function(char) 
                                     local tokenlen = 1
-                                    local sbuff = StrBuffer.new()                           
+                                    local sbuff = StrBuffer.new()
+                                    
                                     while (string.find(char, '%a') ~= nil) or 
-                                          (string.find(char, '%d') ~= nil) or 
-                                          (char == "_") do
+                                        (string.find(char, '%d') ~= nil) or 
+                                        (char == "_") do
                                         
                                         if tokenlen > TunableParameters.MAXSTRING then
                                             error("identifier too long!")
                                         end             
                                         
                                         sbuff:append(char)
-                                        tokenlen = tokenlen + 1
-                                        
-                                        char = scan:nextChar()
-                                       
+                                        tokenlen = tokenlen + 1                                       
+                                        char = scan:nextChar()                                       
                                     end
-                                   
+                                
                                     
                                     return Token.new(TokVal.IDENT,sbuff:toString())
                                 end,
-                                
+        
+        --[[
+            Rules for variables:
+        --]]
         ['VARIABLE']        =   function(char) 
                                     local tokenlen = 1
                                     local sbuff = StrBuffer.new()
                                     while (string.find(char, '%a') ~= nil) or 
-                                          (string.find(char, '%d') ~= nil) or
-                                          (char == "_") do
+                                        (string.find(char, '%d') ~= nil) or
+                                        (char == "_") do
                                         
                                         if tokenlen > TunableParameters.MAXSTRING then
                                             error("identifier too long!")
@@ -259,59 +109,89 @@ function Rules.new()
                                         tokenlen = tokenlen + 1
                                         
                                         char = scan:nextChar()
-                                       
+                                    
                                     end
                                     
                                     return Token.new(TokVal.VARIABLE, sbuff:toString())
                                 end,
-                                    
+        --[[
+            Rules for numbers:
+        --]]
         ["NUMBER"]          =   function(char)
                                     local num = StrBuffer.new()
-                                   
+                                
                                     while string.find(char, '%d') ~= nil do
                                         num:append(char)
                                         char = scan:nextChar()
                                     end
                                     local k = num.toString()
-                                   
+                                
                                     --[[
-                                        lua provides auto conversion between strings and numbers
+                                        lua provides auto conversion between st    -- Load a file into the scanner
+    scan:loadFile("test.pp")rings and numbers
                                         at runtime, so no need to convert string to number before
                                         creating the following token.
                                     --]]
                                     return Token.new(TokVal.NUMBER, num:toString())
                                 end,
-                                
+        
+        --[[
+            Right parenthesis:
+        --]]
         ["("]               =   function() 
                                     scan:nextChar()
                                     return Token.new(TokVal.LPAR,"(")
                                 end,
-                                
+        
+        --[[
+            Left parenthesis:
+        --]]
         [")"] = function() 
                     scan:nextChar()
                     return Token.new(TokVal.RPAR,")")                         
                 end,
                 
+        --[[
+            Comma:
+        --]]
         [","] = function() 
                     scan:nextChar()
                     return Token.new(TokVal.COMMA,",")
                 end,
-                
+        
+        --[[
+            Dot:
+        --]]
         ["."] = function() 
                     scan:nextChar()
                     return Token.new(TokVal.DOT,".")       
                 end,
                 
+        --[[
+            Equals:
+        --]]
         ["="] = function() 
                     scan:nextChar()
                     return Token.new(TokVal.EQUAL,"=")     
                 end,
+        
+        --[[
+            Exclamation:
+        --]]
         ["!"] = function() 
                     scan:nextChar()
                     --[[ TODO tokval = cutsym --]] 
                     return Token.new(TokVal.IDENT,"!") 
                     end,
-                    
+        
+        --[[
+            Forward slash:
+                                    lineno = 
+            If we see this character, check the next. If that is not a "*", then 
+            throw an error for the "/" character. Else loop until we find either then
+            closing symbol of a comment (*/) or the end of the character stream, throw
+            an error in the latter case.
+        --]]
         ["/"] = function() 
                     local char = scan:nextChar()
                     if char ~= "*" then
@@ -330,7 +210,10 @@ function Rules.new()
                     end       
                     return Token.new(0,0)
                 end,
-                
+        
+        --[[
+            Colon - (incorporates rules for ":-"):
+        --]]
         [":"] = function(char)
                     local token = 0
                     char = scan:nextChar()
@@ -342,12 +225,14 @@ function Rules.new()
                     end
                     return token
                 end,
-                
+        --[[
+            Single quote (') - incorporates rules for characters in lprolog e.g 'a':
+        --]]
         ["\'"] = function()
                         local char = ''
                         -- TODO tokival = ascii rep of char ~IMPROVE
                         char = scan:nextChar() 
-                                              
+                                            
                         if scan:nextChar()   ~= "\'" then
                             -- throw syntax error
                             error("missing quote!")
@@ -356,7 +241,10 @@ function Rules.new()
                         
                         return Token.new(TokVal.CHCON, char)
                     end,
-                    
+        
+        --[[
+            Double quote (") - incorporates rules for string in lprolog e.g "hello":
+        --]]
         ["\""] = function()                       
                         local char = scan:nextChar()
                         while char ~= "\"" and char ~= SpecVals.ENDLINE do
@@ -371,38 +259,83 @@ function Rules.new()
                         return Token.new(TokVal.STRCON,sbuff)
                     end,
                     
+        --[[
+            Illegal characters:
+        --]]
         ['ILLEGAL'] =   function(char) 
                             -- TODO kill interpreter
                             error("illegal character " .. char .." on line ".. lineno )                             
                         end
-                   
+                
     }
     
+    -- Public Functions
+    --[[
+        @return the table representing the rules for characters.
+    --]]
     function self:rules() return rules end
     
-    function func(char) 
+    -- Private Functions
+    --[[
+        Refers to a specific index in the rules table, depending on
+        what the value of 'char' is.
+        @param char -
+    --]]
+    local function ruleMatcher(char) 
         local ret
+        -- uppercase character
         if string.find(char, '%u') ~= nil then
-            ret =  rules['VARIABLE'](char)        
+            ret = rules['VARIABLE'](char)
+        
+        -- lowercase character
         elseif string.find(char, '%a') ~= nil then
             ret = rules['IDENT'](char)
+        
+        -- digit
         elseif string.find(char, '%d') then
             ret = rules['NUMBER'](char)
-        else
+        
+        --illegal
+        else               
             ret = rules['ILLEGAL'](char)
         end
         return ret
     end
     
-    tlib.setDefault(rules, func)
+    -- Initialization
+    --[[
+        Sets the default value that rules will return if an non-existant 
+        index tries to be accessed.
+    --]]
+    _tlib.setDefault(rules, ruleMatcher)
     
     
     return self
 end
---scan:printAll()
+
+
+LexicalAnalyzer = {}
+function LexicalAnalyzer.new()
+    local self = {}
+    local lineno = 1
+    local rules = Rules.new()
+    
+    function self:getNextToken()
+        local token
+        local char = scan:currentChar()
+        token = rules:rules()[char](char)
+        return token
+    end
+    
+    return self
+end
+------------------------
+-- Testing --
 local t = -1
+local lexer = LexicalAnalyzer.new()
+
 while t ~= TokVal.EOFTOK do
-    t = getToken2()
+    t = lexer:getNextToken()
     val = t:getValue() 
     if val == nil then val = "nil" end
     print("type= " .. t:getType() .. " | value= " .. val)
